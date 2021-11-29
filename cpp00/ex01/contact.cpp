a8f31361774c4ad1dedd9b7145389fdc767a261e
yeawonbong <ybong.develop@gmail.com>
2021-11-29 21:01:48 +0900

    1129

--- cpp00/ex01/contact.cpp
+++ cpp00/ex01/contact.cpp
@@ -2,13 +2,22 @@
 
 class Contact
 {
-	private:
+private:
 	std::string	first_name;
 	std::string	last_name;
 	std::string	nickname;
 	std::string	phone_number;
 	std::string	darkest_secret;
+	int	order=0;
 
-	public:
+public:
+	Contact(int order)
+	{
+		this.order = order;
+	}
+	void	new_contact()
+	{
+		
+	}
 	
 };
