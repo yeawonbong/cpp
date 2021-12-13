c71a264f369cff3a9d12199aa7def3b675d3b696
yeawonbong <ybong.develop@gmail.com>
2021-12-13 19:18:06 +0900

    1213_ex0304

---
+++ cpp01/ex04/src/main.cpp
@@ -0,0 +1,18 @@
+#include "replace.hpp"
+
+int	main(int argc, char* argv[])
+{
+	std::ifstream	fin;
+
+	if (argc != 4)
+	{
+		std::cout << "Invalid Input!" << std::endl;
+		return (0);
+	}
+	fin.open(argv[1], std::ios_base::in);
+	if (!fin.is_open())
+	{
+		std::cout << "Invalid Filename!" << std::endl;
+		return (0);
+	}
+}
