0c89d68dd8158036183de00696258c566c0c5832
yeawonbong <ybong.develop@gmail.com>
2021-12-16 18:37:05 +0900

    1216_cpp0104_process

--- cpp01/ex04/include/replace.hpp
+++ cpp01/ex04/include/replace.hpp
@@ -4,4 +4,5 @@
 # include <iostream>
 # include <fstream>
 
+# define BUFFER_SIZE 1000
 #endif
--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -1,22 +1,37 @@
 #include "replace.hpp"
 
+int	replace(char* argv[])
+{
+	std::ifstream	fileIn; // input file stream
+	std::ofstream	fileOut; // output file stream
+	char	line[BUFFER_SIZE];
+
+	fileIn.open(argv[1], std::ios_base::in);
+	fileOut.open((argv[1] + ".replace"));
+	if (!fileIn.is_open())
+	{
+		std::cout << "Invalid Filename!" << std::endl;
+		return (1);
+	}
+	int pos;
+	while (!fileIn.eof())
+	{
+		fileIn.getline(line, BUFFER_SIZE);
+		std::string str(line);
+		pos = str.find(argv[2],0); // 문자 찾음
+		// ou
+	}
+}
+
 int	main(int argc, char* argv[])
 {
-	std::ifstream	fin;
+
 
 	if (argc != 4)
 	{
 		std::cout << "Invalid Input!" << std::endl;
 		return (0);
 	}
-	fin.open(argv[1], std::ios_base::in);
-	if (!fin.is_open())
-	{
-		std::cout << "Invalid Filename!" << std::endl;
-		return (0);
-	}
-	while (!fin.eof())
-	{
-		
-	}
+
+	
 }
