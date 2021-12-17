cb9373d6aae6e0e369a4e15c95b2c25f48570f5b
yeawonbong <ybong.develop@gmail.com>
2021-12-17 15:08:43 +0900

    1217_ex04

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -1,37 +1,48 @@
-#include "replace.hpp"
+#include "../include/replace.hpp"
 
 int	replace(char* argv[])
 {
 	std::ifstream	fileIn; // input file stream
 	std::ofstream	fileOut; // output file stream
 	char	line[BUFFER_SIZE];
+	char*	ptr;
+	int		pos;
 
 	fileIn.open(argv[1], std::ios_base::in);
-	fileOut.open((argv[1] + ".replace"));
-	if (!fileIn.is_open())
+	fileOut.open((((std::string)argv[1]).append(".replace")));
+	if (!fileIn.is_open() || !fileOut.is_open())
 	{
 		std::cout << "Invalid Filename!" << std::endl;
 		return (1);
 	}
-	int pos;
 	while (!fileIn.eof())
 	{
 		fileIn.getline(line, BUFFER_SIZE);
-		std::string str(line);
-		pos = str.find(argv[2],0); // 문자 찾음
-		// ou
+		ptr = line;
+		while (*ptr)
+		{
+			pos = ((std::string)ptr).find(argv[2],0); // 문자 찾음
+			if (pos == std::string::npos)
+			{
+				fileOut << ptr;
+				break;
+			}
+			fileOut << ((std::string)ptr).substr(0, pos);
+			fileOut << argv[3];
+			ptr += (pos + strlen(argv[2]));
+		}
+		if (!fileIn.eof())
+			fileOut << std::endl;
 	}
+	return (0);
 }
 
 int	main(int argc, char* argv[])
 {
-
-
 	if (argc != 4)
 	{
 		std::cout << "Invalid Input!" << std::endl;
 		return (0);
 	}
-
-	
+	replace(argv);
 }
